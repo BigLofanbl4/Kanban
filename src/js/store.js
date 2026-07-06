091b33ef3573a80c1943e2b7439b0167b29ac668
BigLofanbl4 <smallofanbl4@gmail.com>
2026-07-06 15:47:36 +0300

    Изменение способа хранения данных - Данные хранятся в плоском Map (источник истины), на его основе вычисляется карточки по колонкам; - Добавлены функции создания пустой карточки и обновления карточки;

--- src/js/store.js
+++ src/js/store.js
@@ -1,42 +1,46 @@
-export function addCard(currentMap, newCard) {
-  const newMap = new Map(currentMap);
-  const columnName = newCard.column;
-  const columnCards = newMap.get(columnName);
-
-  newMap.set(columnName, [...columnCards, newCard]);
-
+export function addCard(cardsMap, newCard) {
+  const newMap = new Map(cardsMap);
+  newMap.set(newCard.id, newCard);
   return newMap;
 }
 
-export function removeCard(currentMap, cardId, columnName) {
-  const newMap = new Map(currentMap);
-  const columnCards = newMap.get(columnName);
+export function getColumnsFromCards(cardsMap, columnNames) {
+  const columnsMap = new Map();
+  columnNames.forEach((column) => columnsMap.set(column, []));
+  for (const [cardId, card] of cardsMap) {
+    const columnName = card.column;
+    if (columnNames.includes(columnName)) {
+      const currentColumnCards = columnsMap.get(columnName);
+      columnsMap.set(columnName, [...currentColumnCards, cardId]);
+    }
+  }
+  return columnsMap;
+}
 
-  const updatedCards = columnCards.filter((card) => card.id !== cardId);
-
-  newMap.set(columnName, updatedCards);
-
+export function removeCard(cardsMap, cardId) {
+  const newMap = new Map(cardsMap);
+  newMap.delete(cardId);
   return newMap;
 }
 
-export function getColumnCards(currentMap, columnName) {
-  return currentMap.get(columnName) || [];
+export function getColumnCards(cardsMap, columnsMap) {
+  const columnsCards = new Map();
+  for (const [columnName, cardIds] of columnsMap) {
+    const columnCardsObjs = cardIds.map((id) => cardsMap.get(id));
+    columnsCards.set(columnName, columnCardsObjs);
+  }
+  return columnsCards;
 }
 
-export function changeCardColumn(currentMap, cardId, oldColumn, newColumn) {
-  const sourceColumn = currentMap.get(oldColumn);
-  const targetCard = sourceColumn.find((card) => card.id === cardId);
-
-  const updatedCard = { ...targetCard, column: newColumn };
-
-  const mapWithoudCard = removeCard(currentMap, cardId, oldColumn);
-  const finalMap = addCard(mapWithoudCard, updatedCard);
-
-  return finalMap;
+export function changeCardColumn(cardsMap, cardId, newColumn) {
+  const newMap = new Map(cardsMap);
+  const targetCard = newMap.get(cardId);
+  newMap.set(cardId, { ...targetCard, column: newColumn });
+  return newMap;
 }
 
-export function saveCards(currentMap) {
-  const obj = Object.fromEntries(currentMap);
+export function saveCards(cardsMap) {
+  const obj = Object.fromEntries(cardsMap);
   localStorage.setItem("store", JSON.stringify(obj));
 }
 
@@ -53,4 +57,20 @@ export function loadCards() {
     console.error(err);
     return new Map();
   }
+}
+
+export function createEmptyCard(columnName) {
+  return {
+    id: crypto.randomUUID(),
+    title: "",
+    desc: "",
+    column: columnName,
+  };
+}
+
+export function updateCard(currentMap, cardId, newTitle, newDesc = "") {
+  const newMap = new Map(currentMap);
+  const targetCard = newMap.get(cardId);
+  newMap.set(cardId, { ...targetCard, title: newTitle, desc: newDesc });
+  return newMap;
 }
